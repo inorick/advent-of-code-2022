c71b655c890d91e7acc12ca63fdd1e54a1341cd9
Jawad Ahmad <jawad.ahmad@accenture.com>
2022-12-04 07:31:00 +0100

    Combine the ifs in first case. Couldn't simplify the expression.

--- day04/jawad/main.c
+++ day04/jawad/main.c
@@ -10,19 +10,13 @@ int main(void)
     u32 first_end = 0;
     u32 second_start = 0;
     u32 second_end = 0;
-    u32 elf_counter = 0;
     u32 pair_counter = 0;
     while(fgets(line,sizeof(line),stdin))
     {
         sscanf(line,"%d-%d,%d-%d\n",&first_start,&first_end,&second_start,&second_end);
-        if(first_start <= second_start && first_end >= second_end)
-        {
+        if((first_start <= second_start && first_end >= second_end) ||
+                (first_start >= second_start && first_end <= second_end))
             pair_counter++;
-        }
-        else if(first_start >= second_start && first_end <= second_end)
-        {
-            pair_counter++;
-        }
     }
     printf("first: %d\n",pair_counter);
 
