c1e3b10cf430e924f4e134001ce5bd09614a1daf
Jawad Ahmad <jawad.ahmad@accenture.com>
2022-12-14 12:58:21 +0100

    Day10 part 2.

--- day10/jawad/main.py
+++ day10/jawad/main.py
@@ -18,3 +18,12 @@ for i in range(19,len(xvalues),40):
     sum += (i + 1) * xvalues[i]
 print(sum)
 
+for i in range(len(xvalues)):
+    col = i % 40
+    if col in [xvalues[i]-1,xvalues[i],xvalues[i]+1]:
+        print("#",end='')
+    else:
+        print(" ",end='')
+    if (i+1) % 40 == 0:
+        print("")
+
