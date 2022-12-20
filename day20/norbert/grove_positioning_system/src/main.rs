307f10cc3cd48ca174f7c04436d0b6e8441b09f9
Norbert Fabritius <norbert.fabritius@esrlabs.com>
2022-12-20 12:13:30 +0100

    Cleanup solution for day 20

--- day20/norbert/grove_positioning_system/src/main.rs
+++ day20/norbert/grove_positioning_system/src/main.rs
@@ -10,13 +10,11 @@ pub enum Error {
 }
 
 pub fn solve(input: String, part2: bool) -> Result<i64, Error> {
-    const DECRYPTION_KEY: i64 = 811589153;
-    let effective_key = if part2 { DECRYPTION_KEY } else { 1 };
-
+    let decryption_key = if part2 { 811589153 } else { 1 };
     let mut numbers: Vec<_> = input
         .lines()
         .filter_map(|line| line.parse::<i64>().ok())
-        .map(|n| n * effective_key)
+        .map(|n| n * decryption_key)
         .enumerate()
         .collect();
     let modulus = numbers.len();
@@ -26,15 +24,15 @@ pub fn solve(input: String, part2: bool) -> Result<i64, Error> {
         for initial_i in 0..modulus {
             let src_i = numbers.iter().position(|(i, _)| *i == initial_i).unwrap();
             let number = numbers.remove(src_i);
-            let dest_i = modulo_pos((src_i as i64) + number.1, modulus - 1);
+            let dest_i = ix_mod((src_i as i64) + number.1, modulus - 1);
             numbers.insert(dest_i, number);
         }
     }
 
     let zero_index = numbers.iter().position(|n| n.1 == 0).ok_or(InvalidInput)?;
-    let i1 = modulo_pos((zero_index + 1000) as i64, modulus);
-    let i2 = modulo_pos((zero_index + 2000) as i64, modulus);
-    let i3 = modulo_pos((zero_index + 3000) as i64, modulus);
+    let i1 = ix_mod((zero_index + 1000) as i64, modulus);
+    let i2 = ix_mod((zero_index + 2000) as i64, modulus);
+    let i3 = ix_mod((zero_index + 3000) as i64, modulus);
     let n1 = numbers.get(i1).unwrap().1;
     let n2 = numbers.get(i2).unwrap().1;
     let n3 = numbers.get(i3).unwrap().1;
@@ -42,8 +40,10 @@ pub fn solve(input: String, part2: bool) -> Result<i64, Error> {
     Ok(n1 + n2 + n3)
 }
 
-fn modulo_pos(n: i64, m: usize) -> usize {
-    ((n % m as i64) + m as i64) as usize % m
+/// Calculate r = n % m such that 0 <= r < m
+fn ix_mod(n: i64, m: usize) -> usize {
+    let m = m as i64;
+    (((n % m) + m) % m) as usize
 }
 
 #[cfg(test)]
