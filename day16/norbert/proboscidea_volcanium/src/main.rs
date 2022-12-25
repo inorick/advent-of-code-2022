e32c5cde3203cd5503b14b10663929113fec2041
Norbert Fabritius <n.fabritius@posteo.de>
2022-12-25 19:15:15 +0100

    Redo solution for day 16

--- day16/norbert/proboscidea_volcanium/src/main.rs
+++ day16/norbert/proboscidea_volcanium/src/main.rs
@@ -1,8 +1,11 @@
 extern crate core;
 
 use crate::Error::InvalidInput;
-use std::cmp::{max, min};
-use std::collections::HashSet;
+use itertools::Itertools;
+use pathfinding::prelude::{bfs, dijkstra_all};
+use std::cmp::max;
+use std::collections::hash_map::Entry;
+use std::collections::{HashMap, HashSet};
 
 fn main() {}
 
@@ -10,42 +13,177 @@ fn main() {}
 pub enum Error {
     InvalidInput,
 }
-type Name = String;
-type Flow = u32;
-type Distance = u32;
 
-#[derive(Clone)]
-pub struct Node {
-    pub name: Name,
-    pub flow_rate: Flow,
-    pub neighbours: HashSet<Name>, // Directly reachable nodes
-    pub connections: HashSet<(Name, Distance)>, // Indirectly reachable nodes with strict. pos. flow
-}
-
-impl Eq for Node {}
-
-impl PartialEq<Self> for Node {
-    fn eq(&self, other: &Self) -> bool {
-        self.name == other.name
-    }
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct State {
+    name: String,
+    open_nodes: Vec<String>,
+    time_elapsed: u32,
+    released: u32,
+    flow: u32,
 }
 
 const START: &str = "AA";
 
-pub fn solve(input: String, part2: bool) -> Result<u32, Error> {
-    let mut nodes = vec![];
+pub fn solve(input: String) -> Result<u32, Error> {
+    let (names, flows, neighbours) = parse(input)?;
+    let connections = connections(&names, &flows, neighbours);
+    let max_released = max_released(&flows, &connections);
+    Ok(max_released)
+}
 
-    // Parse nodes
+fn max_released(
+    flows: &HashMap<String, u32>,
+    connections: &HashMap<String, Vec<(String, u32)>>,
+) -> u32 {
+    const MAX_MINUTES: u32 = 30;
+    let mut max_released = 0;
+    loop {
+        let goal = max_released + 1;
+        let result = find_solution(flows, connections, goal, MAX_MINUTES);
+        match result {
+            None => {
+                println!("No solution for pressure goal {goal}");
+                break;
+            }
+            Some(result) => {
+                max_released = goal;
+                println!("Solution for pressure goal {goal} found:");
+                for state in result {
+                    println!("{state:?}");
+                }
+            }
+        }
+        println!();
+    }
+    max_released
+}
+
+fn find_solution(
+    flows: &HashMap<String, u32>,
+    connections: &HashMap<String, Vec<(String, u32)>>,
+    goal: u32,
+    max_minutes: u32,
+) -> Option<Vec<State>> {
+    let mut max_released = 0;
+    bfs(
+        &State {
+            name: START.to_string(),
+            open_nodes: vec![],
+            time_elapsed: 0,
+            released: 0,
+            flow: 0,
+        },
+        |state| {
+            let mut successors = vec![];
+            if state.time_elapsed > max_minutes {
+                panic!("time_elapsed > MAX_MINUTES");
+            }
+            if state.time_elapsed == max_minutes {
+                return successors;
+            }
+            let max_future_released = max_future_released(state, flows, max_minutes);
+            if state.released + max_future_released < max_released {
+                return successors;
+            }
+            // Open current node
+            if !state.open_nodes.contains(&state.name) {
+                let mut opened = state.clone();
+                opened.open_nodes.push(state.name.clone());
+                opened.released += opened.flow;
+                max_released = max(max_released, opened.released);
+                opened.flow += flows.get(&state.name).expect("failed to get flow");
+                opened.time_elapsed += 1;
+                successors.push(opened);
+            }
+            // Visit connected nodes
+            for (name, dist) in connections
+                .get(&state.name)
+                .expect("failed to get connection")
+                .iter()
+            {
+                if state.open_nodes.contains(name) {
+                    continue;
+                }
+                if state.time_elapsed + dist + 1 > max_minutes {
+                    continue;
+                }
+                let mut visit = state.clone();
+                visit.name = name.clone();
+                visit.time_elapsed += dist;
+                visit.released += state.flow * dist;
+                successors.push(visit);
+            }
+            // Do nothing
+            let mut stay = state.clone();
+            stay.time_elapsed += 1;
+            stay.released += state.flow;
+            successors.push(stay);
+            successors
+        },
+        |state| state.released >= goal,
+    )
+}
+
+fn connections(
+    names: &HashSet<String>,
+    flows: &HashMap<String, u32>,
+    neighbours: HashMap<String, HashSet<String>>,
+) -> HashMap<String, Vec<(String, u32)>> {
+    let mut connections = HashMap::new();
+    for start in names {
+        let reachables = dijkstra_all(start, |name| {
+            neighbours
+                .get(name)
+                .expect("failed to get neighbour")
+                .iter()
+                .map(|neighbour| (neighbour.clone(), 1))
+                .collect::<Vec<(String, u32)>>()
+        });
+        for (dest, (_, distance)) in &reachables {
+            if *flows.get(dest).expect("failed to get flow") > 0 {
+                match connections.entry(start.clone()) {
+                    Entry::Vacant(e) => {
+                        e.insert(vec![(dest.clone(), *distance)]);
+                    }
+                    Entry::Occupied(mut e) => {
+                        e.get_mut().push((dest.clone(), *distance));
+                    }
+                }
+            }
+        }
+        print!("Node {}: ", start);
+        for (dest, dist) in connections.get(start).expect("failed to get connection") {
+            print!("({dest}, {dist}) ");
+        }
+        println!();
+    }
+    connections
+}
+
+fn parse(
+    input: String,
+) -> Result<
+    (
+        HashSet<String>,
+        HashMap<String, u32>,
+        HashMap<String, HashSet<String>>,
+    ),
+    Error,
+> {
     let lines: Vec<Vec<_>> = input
         .lines()
         .map(|line| line.split(' ').collect())
         .collect();
+    let mut names = HashSet::new();
+    let mut flows = HashMap::new();
+    let mut neighbours = HashMap::new();
     for line in lines {
         let name = line[1].to_string();
-        let mut rate = line[4].to_string();
-        rate.retain(|c| c.is_numeric());
-        let rate = rate.parse::<Flow>().map_err(|_| InvalidInput)?;
-        let neighbours: HashSet<_> = line[9..]
+        let mut flow = line[4].to_string();
+        flow.retain(|c| c.is_numeric());
+        let flow = flow.parse::<u32>().map_err(|_| InvalidInput)?;
+        let nb: HashSet<_> = line[9..]
             .iter()
             .map(|s| {
                 let mut s = s.to_string();
@@ -53,261 +191,35 @@ pub fn solve(input: String, part2: bool) -> Result<u32, Error> {
                 s
             })
             .collect();
-        nodes.push(Node {
-            name,
-            flow_rate: rate,
-            neighbours,
-            connections: HashSet::new(),
-        })
+        names.insert(name.clone());
+        flows.insert(name.clone(), flow);
+        neighbours.insert(name, nb);
     }
-    let nodes = nodes; // Make immutable
-
-    // Create network of nodes with the starting node and all other notes with a flow rate > 0
-    let mut network: Vec<_> = nodes
-        .iter()
-        .filter(|n| n.flow_rate > 0 || n.name == START)
-        .cloned()
-        .collect();
-    for start in &mut network {
-        let mut visited = HashSet::new();
-        visited.insert(start.name.clone());
-        let mut front = HashSet::new();
-        start.neighbours.iter().for_each(|n| {
-            front.insert(n.clone());
-        });
-        let mut distance = 1;
-        while !front.is_empty() {
-            let mut next_front = HashSet::new();
-            for front_node in &front {
-                let front_node = nodes
-                    .iter()
-                    .find(|n| &n.name == front_node)
-                    .ok_or(InvalidInput)?;
-                if front_node.flow_rate > 0 {
-                    start
-                        .connections
-                        .insert((front_node.name.clone(), distance));
-                }
-                for neighbour in &front_node.neighbours {
-                    if !visited.contains(neighbour) && !front.contains(neighbour) {
-                        next_front.insert(neighbour.clone());
-                    }
-                }
-                visited.insert(front_node.name.clone());
-            }
-            front = next_front;
-            distance += 1;
-        }
-
-        // Print network
-        print!("Node {}: ", start.name);
-        for (name, distance) in &start.connections {
-            print!("({name}, {distance}) ");
-        }
-        println!();
-    }
-
-    // Solve network resulting network
-    if part2 {
-        visit_with_elephant(
-            START.to_string(),
-            START.to_string(),
-            0,
-            0,
-            network.clone(),
-            0,
-            0,
-            0,
-        )
-    } else {
-        visit_alone(START.to_string(), network.clone(), 0, 0, 0)
-    }
+    Ok((names, flows, neighbours))
 }
 
-pub fn visit_alone(
-    name: String,
-    mut open_nodes: Vec<Node>,
-    minute: u32,
-    mut flow_rate: u32,
-    mut released: u32,
-) -> Result<u32, Error> {
-    const MAX_MINUTES: u32 = 30;
-    let mut max_released = released;
-
-    // Find self
-    let own_node = open_nodes
-        .iter_mut()
-        .find(|n| n.name == name)
-        .ok_or(InvalidInput)?;
-    let node_rate = own_node.flow_rate;
-    let connections = own_node.connections.clone();
-
-    // Open local valve. Time expense was already allocated during travel planning.
-    if node_rate > 0 {
-        open_nodes.retain(|n| n.name != name);
-        flow_rate += node_rate;
-    }
-
-    if minute >= MAX_MINUTES {
-        panic!("minute={minute}!");
-    }
-
-    // Go somewhere else
-    for (name, distance) in connections {
-        let time_for_travel_and_opening = distance + 1;
-        if minute + time_for_travel_and_opening < MAX_MINUTES
-            && open_nodes.iter().any(|n| n.name == name)
-        {
-            let max_released_by_visit = visit_alone(
-                name,
-                open_nodes.clone(),
-                minute + time_for_travel_and_opening,
-                flow_rate,
-                released + (time_for_travel_and_opening * flow_rate),
-            )?;
-            max_released = max(max_released, max_released_by_visit);
-        }
-    }
-
-    // Wait until time runs out
-    released += (MAX_MINUTES - minute) * flow_rate;
-    max_released = max(max_released, released);
-
-    Ok(max_released)
-}
-
-pub fn visit_with_elephant(
-    my_node_name: String,
-    el_node_name: String,
-    my_tto: u32, // Time it take for me to open remote valve
-    el_tto: u32, // Time it take for the elephant to open remote valve
-    mut open_nodes: Vec<Node>,
-    minute: u32,
-    mut flow_rate: u32,
-    mut released: u32,
-) -> Result<u32, Error> {
-    if my_node_name != START && my_node_name == el_node_name {
-        panic!("Should never visit the same node");
-    }
-
-    const MAX_MINUTES: u32 = 26;
-    let mut max_released = released;
-
-    // Find my node
-    let my_node = open_nodes
+fn max_future_released(state: &State, flows: &HashMap<String, u32>, max_minutes: u32) -> u32 {
+    let minutes_remaining = max_minutes - state.time_elapsed;
+    let mut potential_flows = flows
         .iter()
-        .find(|n| n.name == my_node_name)
-        .ok_or(InvalidInput)?;
-    let my_node_rate = my_node.flow_rate;
-    let my_connections = my_node.connections.clone();
-
-    // Find elephant node
-    let el_node = open_nodes
-        .iter()
-        .find(|n| n.name == el_node_name)
-        .ok_or(InvalidInput)?;
-    let el_node_rate = el_node.flow_rate;
-    let el_connections = el_node.connections.clone();
-
-    // Open local valves
-    let i_arrived = my_tto == 0;
-    let el_arrived = el_tto == 0;
-    if i_arrived && my_node_rate > 0 {
-        open_nodes.retain(|n| n.name != my_node_name);
-        flow_rate += my_node_rate;
-    }
-    if el_arrived && el_node_rate > 0 {
-        open_nodes.retain(|n| n.name != el_node_name);
-        flow_rate += el_node_rate;
-    }
-
-    if minute >= MAX_MINUTES {
-        panic!("minute={minute}!");
-    }
-
-    // Go somewhere else
-    if i_arrived && el_arrived {
-        for my_conn in &my_connections {
-            for el_conn in &el_connections {
-                if my_conn.0 == el_conn.0 {
-                    continue; // Never go to the same node
-                }
-                let time_until_next_open = min(my_conn.1 + 1, el_conn.1 + 1);
-                if minute + time_until_next_open < MAX_MINUTES
-                    && open_nodes.iter().any(|n| n.name == my_conn.0)
-                    && open_nodes.iter().any(|n| n.name == el_conn.0)
-                {
-                    let released = released + (time_until_next_open * flow_rate);
-                    let max_released_by_visit = visit_with_elephant(
-                        my_conn.0.clone(),
-                        el_conn.0.clone(),
-                        my_conn.1 + 1 - time_until_next_open,
-                        el_conn.1 + 1 - time_until_next_open,
-                        open_nodes.clone(),
-                        minute + time_until_next_open,
-                        flow_rate,
-                        released,
-                    )?;
-                    max_released = max(max_released, released);
-                    max_released = max(max_released, max_released_by_visit);
+        .filter(|(name, _)| state.open_nodes.contains(name))
+        .map(|(_, flow)| flow)
+        .sorted()
+        .rev();
+    let mut add_flow = 0;
+    let mut add_released = 0;
+    for minute in 0..minutes_remaining {
+        add_released += state.flow + add_flow;
+        if minute % 2 == 0 {
+            match potential_flows.next() {
+                None => {}
+                Some(flow) => {
+                    add_flow += flow;
                 }
             }
         }
-    } else if i_arrived {
-        for my_conn in &my_connections {
-            if my_conn.0 == el_node_name {
-                continue; // Never go to the same node
-            }
-            let time_until_next_open = min(my_conn.1 + 1, el_tto);
-            if minute + time_until_next_open < MAX_MINUTES
-                && open_nodes.iter().any(|n| n.name == my_conn.0)
-            {
-                let released = released + (time_until_next_open * flow_rate);
-                max_released = max(max_released, released);
-                let max_released_this_way = visit_with_elephant(
-                    my_conn.0.clone(),
-                    el_node_name.clone(),
-                    my_conn.1 + 1 - time_until_next_open,
-                    el_tto - time_until_next_open,
-                    open_nodes.clone(),
-                    minute + time_until_next_open,
-                    flow_rate,
-                    released,
-                )?;
-                max_released = max(max_released, max_released_this_way);
-            }
-        }
-    } else if el_arrived {
-        for el_conn in &el_connections {
-            if el_conn.0 == my_node_name {
-                continue; // Never go to the same node
-            }
-            let time_until_next_open = min(my_tto, el_conn.1 + 1);
-            if minute + time_until_next_open < MAX_MINUTES
-                && open_nodes.iter().any(|n| n.name == el_conn.0)
-            {
-                let released = released + (time_until_next_open * flow_rate);
-                max_released = max(max_released, released);
-                let max_released_this_way = visit_with_elephant(
-                    my_node_name.clone(),
-                    el_conn.0.clone(),
-                    my_tto - time_until_next_open,
-                    el_conn.1 + 1 - time_until_next_open,
-                    open_nodes.clone(),
-                    minute + time_until_next_open,
-                    flow_rate,
-                    released,
-                )?;
-                max_released = max(max_released, max_released_this_way);
-            }
-        }
     }
-
-    // Wait until time runs out
-    released += (MAX_MINUTES - minute) * flow_rate;
-    max_released = max(max_released, released);
-
-    Ok(max_released)
+    add_released
 }
 
 #[cfg(test)]
@@ -317,17 +229,8 @@ pub mod test {
         let mut path = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
         path.push("resources/example_input.txt");
         let input = std::fs::read_to_string(&path).expect("failed to read file");
-        let max_released = crate::solve(input, false).expect("failed to solve");
+        let max_released = crate::solve(input).expect("failed to solve");
         assert_eq!(max_released, 1651);
-    }
-
-    #[test]
-    fn solve_example_with_elephant() {
-        let mut path = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-        path.push("resources/example_input.txt");
-        let input = std::fs::read_to_string(&path).expect("failed to read file");
-        let max_released = crate::solve(input, true).expect("failed to solve");
-        assert_eq!(max_released, 1707);
     }
 
     #[test]
@@ -335,16 +238,7 @@ pub mod test {
         let mut path = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
         path.push("resources/input.txt");
         let input = std::fs::read_to_string(&path).expect("failed to read file");
-        let max_released = crate::solve(input, false).expect("failed to solve");
+        let max_released = crate::solve(input).expect("failed to solve");
         assert_eq!(max_released, 1641);
-    }
-
-    #[test]
-    fn solve_part2() {
-        let mut path = std::path::PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-        path.push("resources/input.txt");
-        let input = std::fs::read_to_string(&path).expect("failed to read file");
-        let max_released = crate::solve(input, true).expect("failed to solve");
-        assert_eq!(max_released, 0);
     }
 }
